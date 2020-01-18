9a8d2d874aea719c49e66318dafbdd53a8153a0a
douglasmasho <douglasmasho@gmail.com>
2020-01-18 17:32:11 +0200

    Fourth commit
    Further optimization of Kronos

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+var some = document.getElementById("nav-child");
+
+// query = window.matchMedia("(max-width: 900px)");
+
+// if(query.matches){
+//   document.querySelector(".card__hover").style.display = "none";
+// }
+
+function clickLabel (){
+    document.getElementById("label").click() ;
+  }
+
+//   var query = window.matchMedia("(max-width: 75em)");
+
